92c7cc21fd8084b4e4f7c472024cf4b11156c12d
coannanaufal-debug <coanna.naufal@gmail.com>
2026-05-29 14:43:27 +0700

    menambahkan logika iterasi untuk menampilkan elemen antrian saat FRONT > REAR

--- tugasc.cpp
+++ tugasc.cpp
@@ -98,3 +98,10 @@ class Queues
             cout << endl;
         }
         else
+        {
+            // jika FRONT > REAR, iterasi dari FRONT hingga akhir array, lalu dari awal array hingga REAR
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
