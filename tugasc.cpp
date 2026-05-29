391dfd699fbbf8627d75066da75d8480b2c194aa
coannanaufal-debug <coanna.naufal@gmail.com>
2026-05-29 14:41:33 +0700

    memperbaiki logika penanganan antrian pada metode insert

--- tugasc.cpp
+++ tugasc.cpp
@@ -36,3 +36,13 @@ class Queues
             REAR = 0;
         }
         else
+
+        {
+            // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
